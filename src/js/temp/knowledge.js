a7a4b72daf5b038908bf8bed0b12fc9f5b77beb2
seancy <115114@163.com>
2015-12-23 10:14:57 +0800

    add node in knowledge module.

--- src/js/temp/knowledge.js
+++ src/js/temp/knowledge.js
@@ -1,6 +1,17 @@
 define(function(){
     var $el = $('<div><span>this is knowledge page.</span><input type="button" value="new tab" /></div>');
-        
+    
+    /*
+    功能：
+        该方法初始化回调函数，每个业务模块都需有这个函数，用来接收框架信息
+    参数：
+        index 框架页对象，从它这里可以访问到框架各子模块
+            client 左侧客户列表
+            clientIntro 右上角客户信息介绍
+            main 右侧选项卡组的容器
+    返回：
+        必需为jquery dom对象，框架会将此dom添加到选项卡内容区域
+    */
     var initialize = function(index){
         $el.on('click', 'input', function(e){
             console.log(index.main)
