9c21b2e77fddbe2a74c17c479e06980f36a8a513
seancy <115114@163.com>
2015-12-23 10:07:12 +0800

    add index param of system param of tab control.

--- src/js/temp/knowledge.js
+++ src/js/temp/knowledge.js
@@ -1,7 +1,15 @@
 define(function(){
-    var objClass = function(){
+    var $el = $('<div><span>this is knowledge page.</span><input type="button" value="new tab" /></div>');
+        
+    var initialize = function(index){
+        $el.on('click', 'input', function(e){
+            console.log(index.main)
+            index.main.createTab('businessTab', 'js/temp/knowledge');
+        });
 
+        this.content = $el;
+        return $el;
     }
-
-    return { content:'this is knowledge page.' }
+    return initialize;
+    //return { content:$el }
 });
